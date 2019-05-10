0e7f194d9dacb3cead6318412e47f58a9fa33599
llmooon <llmooon@naver.com>
2019-05-10 17:48:29 +0900

    updage first setting

---
+++ server1/src/main/java/com/naver/api/testController.java
@@ -0,0 +1,14 @@
+package com.naver.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class testController {
+    @GetMapping
+    public String test(){
+        return "hello";
+    }
+}
